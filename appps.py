e007ef90c3cbbeba1f5709d53e1ace221cba2283
perfectawak <godsfavourawak954@gmail.com>
2026-02-17 16:21:52 +0100

    Godsfavour

--- appps.py
+++ appps.py
@@ -3,7 +3,7 @@ name = input("what is your name: ")
 print("it is nice to meet you ", name)
 answer = input("Are you enjoying the course?: ")
 if answer.upper == "YES":
-    print("That is good to hear")
+    print("That is good to hear from you my sweet heart i will get back to you asw soon as possible")
 else:
     print("oh no that's sad to hear")
 
